407ec0afad96e6481a42db0d7e266b16a1627469
vandemjh <jack.vandem@gmail.com>
2020-03-21 19:01:22 -0400

    updated to output a matplotlib scatterplot

--- graph.py
+++ graph.py
@@ -15,9 +15,27 @@ for country in timeseries:
 
 
 print (caseTotals)
+dates = caseTotals.keys()
+cases = caseTotals.values()
+dateRange = []
+for i in range(len(dates)):
+    dateRange.append(i)
 
 try:
     import matplotlib.pyplot as plt
 except:
     print("\tIt doesn't look like you have matplotlib installed!\n\tThat's fine, the results of the scrape are exported to file.\n\tYou can also install it using \"pip3 install matplotlib\"")
     quit()
+
+axesSettings = [.11,.11,.8,.8]
+
+fig = plt.figure()
+ax = fig.add_axes(axesSettings)
+ax.scatter(dateRange, cases)
+ax.set_xlabel("Dates")
+ax.set_ylabel("Number of Cases")
+# plt.suptitle('Number of occurances by code fragement (10% of values)', fontsize = 12)
+ax.set_title("Number of cases by date")
+# plt.show()
+plt.savefig("covid")
+plt.close(fig)
