c1da38dbe49da8431f5d418d6f50c80f48741730
vandemjh <jack.vandem@gmail.com>
2020-03-21 20:50:09 -0400

    added dates on x axis

--- graph.py
+++ graph.py
@@ -19,23 +19,24 @@ for country in timeseries:
 # print (caseTotals)
 dates = caseTotals.keys()
 cases = caseTotals.values()
-dateRange = []
-for i in range(len(dates)):
-    dateRange.append(i)
 
 try:
     import matplotlib.pyplot as plt
+    import matplotlib.ticker as ticker
 except:
     print("\tIt doesn't look like you have matplotlib installed!\n\tYou can install it using \"pip3 install matplotlib\"")
     quit()
 
-axesSettings = [.135,.135,.8,.8]
+axesSettings = [.167,.167,.7,.7]
 
 fig = plt.figure()
 ax = fig.add_axes(axesSettings)
-ax.scatter(dateRange, cases)
-ax.set_xlabel("Days since January 22nd")
+ax.scatter(range(len(dates)), cases)
+ax.set_xlabel("Dates")
 ax.set_ylabel("Number of Cases")
+plt.xticks(range(len(dates)), dates, size="small", rotation="45")
+ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
+
 # plt.suptitle('Number of occurances by code fragement (10% of values)', fontsize = 12)
 ax.set_title("Number of cases over time")
 # plt.show()
