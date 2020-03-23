f7ac19d051e39a3b41f268eb25a3aaf568df5bf2
vandemjh <jack.vandem@gmail.com>
2020-03-23 09:08:34 -0400

    removed year from x axis

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ def skipOver(dates, skipSize):
     toReturn = []
     for date in list(dates):
         if (list(dates).index(date) % skipSize == 0):
-            toReturn.append(date)
+            toReturn.append(date[5:len(date)])
         else:
             toReturn.append("")
     return toReturn
