b5b95ab1142270240eedebf29d9151f9f48c7de5
vandemjh <jack.vandem@gmail.com>
2020-03-27 11:40:51 -0400

    added python auto formatting to yml

--- graph.py
+++ graph.py
@@ -7,10 +7,12 @@ import datetime
 from utils import *
 
 # Setting for matplotlib charts
-AXIS_SETTINGS = [.167,.167,.7,.7]
+AXIS_SETTINGS = [0.167, 0.167, 0.7, 0.7]
 
 # timeseries = json.loads(open("timeseries.json","r").read())
-timeseries = json.loads(requests.get("https://pomber.github.io/covid19/timeseries.json").content)
+timeseries = json.loads(
+    requests.get("https://pomber.github.io/covid19/timeseries.json").content
+)
 
 numberOfCountries = 5
 caseTotals = getTotalCasesByDay(timeseries)
@@ -24,12 +26,13 @@ caseTotals = getTotalCasesByDay(timeseries)
 try:
     import matplotlib.pyplot as plt
 except:
-    print("\tIt doesn't look like you have matplotlib installed!\n\tYou can install it using \"pip3 install matplotlib\"")
+    print(
+        '\tIt doesn\'t look like you have matplotlib installed!\n\tYou can install it using "pip3 install matplotlib"'
+    )
     quit()
 
 
 def createScatter(dates, cases, country, fileName):
-
 
     fig = plt.figure()
     ax = fig.add_axes(AXIS_SETTINGS)
@@ -42,10 +45,22 @@ def createScatter(dates, cases, country, fileName):
 
     # plt.suptitle('Number of occurances by code fragement (10% of values)', fontsize = 12)
     dateTime = datetime.datetime.today()
-    ax.set_title("Number of cases in " + country + " as of " + str(dateTime.month) + "-" + str(dateTime.day) + " @ " + str(dateTime.hour) + ":" + (str(dateTime.minute)))
+    ax.set_title(
+        "Number of cases in "
+        + country
+        + " as of "
+        + str(dateTime.month)
+        + "-"
+        + str(dateTime.day)
+        + " @ "
+        + str(dateTime.hour)
+        + ":"
+        + (str(dateTime.minute))
+    )
     # plt.show()
     plt.savefig(fileName)
     plt.close(fig)
+
 
 createScatter(caseTotals.keys(), caseTotals.values(), "total", "covid")
 # print(getCountriesInfectedTotals(timeseries)["Norway"])
@@ -57,12 +72,24 @@ dropdown = ""
 # Updates the Most Infected table
 mostInfected = getTopCountriesInfected(timeseries, COUNTRIES_IN_TABLE)
 count = 0
-                # <a class="dropdown-item" href="countries/">Action</a>
-                # <a class="dropdown-item" href="countries/">Another action</a>
+# <a class="dropdown-item" href="countries/">Action</a>
+# <a class="dropdown-item" href="countries/">Another action</a>
 for country in mostInfected:
     countryCases = getCasesForCountry(timeseries, country)
-    createScatter(countryCases.keys(), countryCases.values(), country, "countries/" + country.lower())
-    dropdown += "<a class=\"dropdown-item\" href=\"countries/" + country.lower() + ".png" + "\">" + country + "</a>"
+    createScatter(
+        countryCases.keys(),
+        countryCases.values(),
+        country,
+        "countries/" + country.lower(),
+    )
+    dropdown += (
+        '<a class="dropdown-item" href="countries/'
+        + country.lower()
+        + ".png"
+        + '">'
+        + country
+        + "</a>"
+    )
     count += 1
     table += "<tr><td>" + str(count) + "</td>"
     table += "<td>" + country + "</td>"
@@ -71,17 +98,26 @@ for country in mostInfected:
 with open("index.html", "r") as html:
     index = str(html.read())
 with open("index.html", "w") as html:
-    startTable = index.find("<!-- TABLE_DATA_START_HERE -->") + len("<!-- TABLE_DATA_START_HERE -->")
+    startTable = index.find("<!-- TABLE_DATA_START_HERE -->") + len(
+        "<!-- TABLE_DATA_START_HERE -->"
+    )
     endTable = index.find("<!-- TABLE_DATA_END_HERE -->")
-    html.write(index[0:startTable] + "\n" + table + "\n" + index[endTable:len(index)])
+    html.write(index[0:startTable] + "\n" + table + "\n" + index[endTable : len(index)])
 
 with open("index.html", "r") as html:
     index = str(html.read())
 with open("index.html", "w") as html:
-    startDropdown = index.find("<!-- DROPDOWN_DATA_START_HERE -->") + len("<!-- DROPDOWN_DATA_START_HERE -->")
+    startDropdown = index.find("<!-- DROPDOWN_DATA_START_HERE -->") + len(
+        "<!-- DROPDOWN_DATA_START_HERE -->"
+    )
     endDropdown = index.find("<!-- DROPDOWN_DATA_END_HERE -->")
-    html.write(index[0:startDropdown] + "\n" + dropdown + "\n" + index[endDropdown:len(index)])
-
+    html.write(
+        index[0:startDropdown]
+        + "\n"
+        + dropdown
+        + "\n"
+        + index[endDropdown : len(index)]
+    )
 
 
 casesByCountry = getCasesByCountry(timeseries)
@@ -89,12 +125,12 @@ fig = plt.figure()
 ax = fig.add_axes(AXIS_SETTINGS)
 
 for country in casesByCountry:
-    if (country in mostInfected):
+    if country in mostInfected:
         dates = list(casesByCountry[country].keys())
         cases = list(casesByCountry[country].values())
         ax.plot(range(len(dates)), cases, label=country)
 
-plt.legend(loc="upper left");
+plt.legend(loc="upper left")
 ax.set_xlabel("Dates")
 ax.set_ylabel("Number of Cases")
 plt.xticks(range(len(dates)), skipOver(list(dates), 3), size="small", rotation="45")
--- utils.py
+++ utils.py
@@ -1,40 +1,45 @@
 import datetime
 
+
 def skipOver(dates, skipSize):
     toReturn = []
     for date in list(dates):
-        if (list(dates).index(date) % skipSize == 0):
-            toReturn.append(date[5:len(date)])
+        if list(dates).index(date) % skipSize == 0:
+            toReturn.append(date[5 : len(date)])
         else:
             toReturn.append("")
     return toReturn
+
 
 # Returns the total cases per day as { "date" : total cases on that date }
 def getTotalCasesByDay(timeseries):
     caseTotals = {}
     for country in timeseries:
         for day in timeseries[country]:
-            if (not day["date"] in caseTotals):
-                caseTotals.update({day["date"] : day["confirmed"]})
+            if not day["date"] in caseTotals:
+                caseTotals.update({day["date"]: day["confirmed"]})
             else:
                 caseTotals[day["date"]] += day["confirmed"]
     return caseTotals
+
 
 # Returns the total cases per day for numberOfCountries as { country : { "date" : total cases on that date } }
 def getCasesByCountry(timeseries):
     toReturn = {}
     for country in timeseries:
         for day in timeseries[country]:
-            if (not country in toReturn):
-                toReturn.update({country : {day["date"] : day["confirmed"]}})
-            else: # (not day["date"] in toReturn[country]):
-                toReturn[country].update({day["date"] : day["confirmed"]})
+            if not country in toReturn:
+                toReturn.update({country: {day["date"]: day["confirmed"]}})
+            else:  # (not day["date"] in toReturn[country]):
+                toReturn[country].update({day["date"]: day["confirmed"]})
             # else:
-                # toReturn[country[day["date"]]] += day["confirmed"]
+            # toReturn[country[day["date"]]] += day["confirmed"]
     return toReturn
+
 
 def getCasesForCountry(timeseries, country):
     return getCasesByCountry(timeseries)[country]
+
 
 # Returns countries as { country : number of cases }
 def getCountriesInfectedTotals(timeseries):
@@ -42,19 +47,27 @@ def getCountriesInfectedTotals(timeseries):
     toIterate = getCasesByCountry(timeseries)
     dateTime = datetime.datetime.today()
     for country in toIterate:
-        if (not country in toReturn):
-            toGet = str(dateTime.year) + "-" + str(dateTime.month) + "-" + str(dateTime.day)
-            if (toGet in toIterate[country]):
-                toReturn.update({country : toIterate[country][toGet]})
+        if not country in toReturn:
+            toGet = (
+                str(dateTime.year) + "-" + str(dateTime.month) + "-" + str(dateTime.day)
+            )
+            if toGet in toIterate[country]:
+                toReturn.update({country: toIterate[country][toGet]})
             else:
                 day = dateTime.day - 1
                 month = dateTime.month
                 year = dateTime.year
                 try:
-                    toGet = str(year if day > 0 and month > 0 else year - 1) + "-" + str(month if day > 0 else month - 1) + "-" + str(day if day > 0 else 1)
-                    toReturn.update({country : toIterate[country][toGet]})
-                except: # Default to yesterdays data
-                    toReturn.update({country : toIterate[country][toGet]})
+                    toGet = (
+                        str(year if day > 0 and month > 0 else year - 1)
+                        + "-"
+                        + str(month if day > 0 else month - 1)
+                        + "-"
+                        + str(day if day > 0 else 1)
+                    )
+                    toReturn.update({country: toIterate[country][toGet]})
+                except:  # Default to yesterdays data
+                    toReturn.update({country: toIterate[country][toGet]})
     # for country in toIterate:
     #     for day in toIterate[country]:
     #         if (not country in toReturn):
@@ -63,13 +76,14 @@ def getCountriesInfectedTotals(timeseries):
     #             toReturn[country] += toIterate[country][day]
     return toReturn
 
+
 # Returns top maxCountries most infected countries as { country : number of cases }
 def getTopCountriesInfected(timeseries, maxCountries):
     toReturn = {}
     toPop = getCountriesInfectedTotals(timeseries)
     for i in range(maxCountries):
-        nextTop = (max(toPop, key=toPop.get))
+        nextTop = max(toPop, key=toPop.get)
         numInfected = toPop[nextTop]
-        toReturn.update({nextTop : numInfected})
+        toReturn.update({nextTop: numInfected})
         toPop.pop(nextTop)
     return toReturn
